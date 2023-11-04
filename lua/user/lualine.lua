fa19c127488daf7d8b9ab6958e8dd4be9fe3f254
anz <jonatas@pop-os.localdomain>
2023-11-03 21:29:04 -0300

    feat(lualine): Change sep symbol

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -43,7 +43,7 @@ function M.config()
       -- component_separators = { left = "", right = "" },
       -- section_separators = { left = "", right = "" },
       component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
+      section_separators = { left = "█", right = "█" },
 
       ignore_focus = { "NvimTree" },
     },
